d65be5e5bbc548750be6e05c562f465b25c8e375
md.lutfar rahman <96845210+devlutfar@users.noreply.github.com>
2022-04-20 15:14:35 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    
+//slick slider
+
+$('.autoplay').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots: true,
+  });
+
+});
